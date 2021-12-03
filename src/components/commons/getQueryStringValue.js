fa80d142c879e7757a8032b02bfa59239a4d3fa2
SoniSSk <kuldeepsoni3099@gmail.com>
2021-12-03 16:12:35 +0530

    aa

--- src/components/commons/getQueryStringValue.js
+++ src/components/commons/getQueryStringValue.js
@@ -1,13 +1,7 @@
 export function getQueryStringValue(key) {
-<<<<<<< HEAD
     return "";
-=======
-//     if (!global.window) {
-//         return "";
-//     }
-//     return decodeURIComponent(window.location.search.replace(new RegExp(`^(?:.*[&\\?]${encodeURIComponent(key).replace(/[\.\+\*]/g, '\\$&')}(?:\\=([^&]*))?)?.*$`, 'i'), '$1'));
-  return "";
->>>>>>> 68be4104a24915e17fb526dfd437b8e935722bc4
+
+    
 }
 
 
